0d2d935c70c74f5e1d891c743d37c05174eb8b60
Alex Shopiak <alshop2004@gmail.com>
2024-12-14 13:34:38 +0000

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Board from "./components/Board";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-    </>
-  )
+    <div className="App">
+      <h1>Minesweeper</h1>
+      <Board />
+    </div>
+  );
 }
 
-export default App
+export default App;
