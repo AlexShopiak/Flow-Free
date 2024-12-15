7a41f81c69d828aa5658121e4fb00dfe499374cb
Alex Shopiak <alshop2004@gmail.com>
2024-12-15 00:34:34 +0000

    Add loop player

--- src/util/player.js
+++ src/util/player.js
@@ -1,6 +1,7 @@
 export const Player = {
     preloadAudio: () => preloadAudio(),
     playSound: (type) => playSound(type),
+    playSoundLoop: (type) => playSoundLoop(type),
     pauseSound: () => pauseSound(), // Добавляем функцию паузы
 };
 
@@ -14,6 +15,12 @@ const sounds = {
     pop_6: "/audio/pop_6.mp3",
     pop_7: "/audio/pop_7.mp3",
     pop_8: "/audio/pop_8.mp3",
+    mine_1: "/audio/mine_1.mp3",
+    mine_2: "/audio/mine_2.mp3",
+    mine_3: "/audio/mine_3.mp3",
+    mine_4: "/audio/mine_4.mp3",
+    mine_5: "/audio/mine_5.mp3",
+    mine_6: "/audio/mine_6.mp3",
     flag_on: "/audio/flag_on.mp3",
     flag_off: "/audio/flag_off.mp3",
     lose: "/audio/lose.mp3",
@@ -32,6 +39,12 @@ const preloadAudio = () => {
 
 const playSound = (type) => {
     currentAudio = new Audio(sounds[type]);
+    currentAudio.play();
+};
+
+const playSoundLoop = (type) => {
+    currentAudio = new Audio(sounds[type]);
+    currentAudio.loop = true; // Включаем повтор
     currentAudio.play();
 };
 
